d6735a458182502d1ada7825a92d061ac05f2512
Jas <penny10335695@gmail.com>
2023-08-26 09:35:41 +0800

    changed from Article(POJO) to ArticlePosting. But still using the same table of Article in SQL

--- src/main/java/tw/idv/cha102/g7/article/controller/ArticlePostingController.java
+++ src/main/java/tw/idv/cha102/g7/article/controller/ArticlePostingController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import tw.idv.cha102.g7.article.entity.Article;
+import tw.idv.cha102.g7.article.entity.ArticlePosting;
 import tw.idv.cha102.g7.article.service.ArticlePostingService;
 @RestController
 public class ArticlePostingController {
@@ -15,7 +15,7 @@ public class ArticlePostingController {
     // 新增文章
     // 可以新增，但若資料庫中有相同id的行程，資料會被覆蓋過去
     @PostMapping("/post")
-    ResponseEntity<?> post(@RequestBody Article article) {
+    ResponseEntity<?> post(@RequestBody ArticlePosting article) {
         try {
             articlePostingService.postArticle(article);
             return ResponseEntity.ok().build();
